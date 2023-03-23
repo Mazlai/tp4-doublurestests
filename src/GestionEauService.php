2a61f0948485755b0802e035a137702bc265b09f
Mazlai <micky31200@gmail.com>
2023-03-23 13:06:41 +0100

    Fini

--- src/GestionEauService.php
+++ src/GestionEauService.php
@@ -1,16 +1,33 @@
+<?php
+
 namespace iut;
+
 use InfoSecheresseService;
 use EtatService;
 use MunicipalServices;
 
-class GestionEauService
-{
+class GestionEauService {
 
-    function __construct(InfoSecheresseService infoSecheresse, EtatService serviceEtat, MunicipalServices servicesMunicipaux) {
+    private InfoSecheresseService $infoSecheresse;
+    private EtatService $serviceEtat;
+    private MunicipalServices $servicesMunicipaux;
+
+    function __construct(InfoSecheresseService $infoSecheresse, EtatService $serviceEtat, MunicipalServices $servicesMunicipaux) {
+        $this->infoSecheresse = $infoSecheresse;
+        $this->serviceEtat = $serviceEtat;
+        $this->servicesMunicipaux = $servicesMunicipaux;
     }
 
     public function checkEtatSecheresse() {
-
+        if ($this->infoSecheresse->previsionDureeSecheresse() > 20 && $this->infoSecheresse->reserveEauMunicipale() < 100.0) {
+            $this->servicesMunicipaux->callLivraisonCiterneEau();
         }
+
+        if($this->infoSecheresse->previsionDureeSecheresse() > 10) {
+            $this->servicesMunicipaux->sendRestrictionEauInformation();
+        } 
     }
+    
 }
+
+?>
--- tests/GestionEauServiceTest.php
+++ tests/GestionEauServiceTest.php
@@ -4,15 +4,49 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 
+use iut\GestionEauService;
+
 use \Mockery\Adapter\Phpunit\MockeryTestCase;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 class GestionEauServiceTest extends MockeryTestCase
 {
-    public function test_()
+    public function testRestrictionEau()
     {
-        $this->assertSame(3, 2 + 2);
+        //GIVEN
+        $mockEau = \Mockery::mock('InfoSecheresseService');
+        $mockEau->shouldReceive('previsionDureeSecheresse')->andReturn(15);
+
+        $mockEtatService = \Mockery::mock('EtatService');
+        $spyMunicipal = \Mockery::spy('MunicipalServices');
+
+        $gestionEau = new GestionEauService($mockEau, $mockEtatService, $spyMunicipal);
+
+        //WHEN
+        $gestionEau->checkEtatSecheresse();
+        
+        //THEN
+        $spyMunicipal->shouldHaveReceived()->sendRestrictionEauInformation();
+    }
+
+    public function testLivraisonEau() {
+        //GIVEN
+        $mockEau = \Mockery::mock('InfoSecheresseService');
+        $mockEau->shouldReceive('previsionDureeSecheresse')->andReturn(25);
+        $mockEau->shouldReceive('reserveEauMunicipale')->andReturn(80.0);
+
+        $mockEtatService = \Mockery::mock('EtatService');
+        $spyMunicipal = \Mockery::spy('MunicipalServices');
+
+        $gestionEau = new GestionEauService($mockEau, $mockEtatService, $spyMunicipal);
+
+        //WHEN
+        $gestionEau->checkEtatSecheresse();
+
+        //THEN
+        $spyMunicipal->shouldHaveReceived()->callLivraisonCiterneEau();
+
     }
 
 }
